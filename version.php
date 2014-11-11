770845e7a2f22cfc157d1b95703b729e2f974d92
Jan Eberhardt <eberhardt@math.tu-berlin.de>
2014-11-11 17:01:34 +0000

    Updated version info
    	modified:   version.php

--- version.php
+++ version.php
@@ -23,6 +23,7 @@
 $plugin->component = "block_sitenews";
 $plugin->requires  = 2014051200;
 $plugin->version   = 2014072200;
-$plugin->maturity  = MATURITY_BETA;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = "1.0 (Build 2014072200)";
 
 ?>
